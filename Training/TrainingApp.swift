77a1db01bb738572b6edbd3e590e9b5f7a5a906a
Redouane Amour <redouane.amour@gmail.com>
2026-07-14 18:49:14 +0200

    Initial Commit

---
+++ Training/TrainingApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrainingApp.swift
+//  Training
+//
+//  Created by Redouane Amour on 14/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct TrainingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
